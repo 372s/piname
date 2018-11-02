8a2e677740b60e543d784d702d813da2fdc47da3
王强 <wangqiang@medlive.cn>
2018-11-02 18:19:08 +0800

    update

--- public/index.php
+++ public/index.php
@@ -1,9 +1,11 @@
 <?php
 require __DIR__.'/../vendor/autoload.php';
 
-// echo 2222;
-$con = new \Controllers\Repository();
-echo $con->get('app.name');die;
-// get('hi', function() {
-//     echo "I am Pinatra framework!";
-// });
+\Pinatra\Routing\Router::$baseNamespace = '\\Controllers\\';
+
+get('/', function() {
+    echo "GET /";
+});
+
+
+get('home', 'HomeController@home');
